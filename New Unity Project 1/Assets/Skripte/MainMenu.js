0803934c0b6d709b4f7c5a52db196260445e97fd
kadir003 <kadir.busatlic@edu.fit.ba>
2015-05-24 17:11:47 +0200

    menu

---
+++ New Unity Project 1/Assets/Skripte/MainMenu.js
@@ -0,0 +1,22 @@
+﻿function StartGame ()
+{
+Application.LoadLevel("ChoseMap");
+}
+function HelpButton ()
+{
+Application.LoadLevel("Help");
+}
+
+function Map1 ()
+{
+Application.LoadLevel("level2");
+}
+
+function Map2 ()
+{
+Application.LoadLevel("level1");
+}
+function ExitGame () 
+{
+Application.Quit();
+}
